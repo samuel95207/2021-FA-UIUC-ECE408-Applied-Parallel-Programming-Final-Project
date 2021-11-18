f1ac16f9b2cd63cca14dc287de1c5d333949a527
Samuel Hunag <lspss95207@gmail.com>
2021-11-17 21:38:34 -0600

    Add commented "Tiled optimization"

--- src/custom/new-forward.cu
+++ src/custom/new-forward.cu
@@ -163,8 +163,8 @@ __global__ void conv_forward_kernel_C4(float *__restrict__ y, const float *__res
     // Calculate Convolution
     if ((w < W_out) && (h < H_out)) {
         float acc = 0.0f;
-        // Unroll for loop
-        #pragma unroll 4
+// Unroll for loop
+#pragma unroll 4
         for (int c = 0; c < 4; c++) {
             acc += x4d(b, c, h + 0, w + 0) * k4d_constant(m, c, 0, 0) +
                    x4d(b, c, h + 0, w + 1) * k4d_constant(m, c, 0, 1) +
@@ -224,6 +224,7 @@ __global__ void conv_forward_kernel_C4(float *__restrict__ y, const float *__res
 #undef x4d
 #undef k4d_constant
 }
+
 
 cudaStream_t stream[STREAM_NUM];
 
@@ -377,3 +378,207 @@ __host__ void GPUInterface::get_device_properties() {
         std::cout << "Warp Size: " << deviceProp.warpSize << std::endl;
     }
 }
+
+
+
+
+/*
+// Tiled version of the code, the result is slower than baseline so I didn't use it
+
+#include <cmath>
+#include <iostream>
+
+#include "gpu-new-forward.h"
+
+#define TILE_WIDTH 26
+#define KERNEL_WIDTH 7
+#define BLOCK_WIDTH (TILE_WIDTH + ((int)KERNEL_WIDTH / 2) * 2)
+#define C_MAX 4
+
+__constant__ float Kernel[4096];
+
+
+__global__ void conv_forward_kernel(float *y, const float *x, const float *k, const int B,
+                                    const int M, const int C, const int H, const int W,
+                                    const int K) {
+
+    __shared__ float x_shared[C_MAX * BLOCK_WIDTH * BLOCK_WIDTH];
+
+
+    const int H_out = H - K + 1;
+    const int W_out = W - K + 1;
+
+#define y4d(i3, i2, i1, i0) \
+    y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+#define x_shared_3d(i2, i1, i0) \
+    x_shared[(i2) * (BLOCK_WIDTH * BLOCK_WIDTH) + (i1) * (BLOCK_WIDTH) + i0]
+#define k4d_constant(i3, i2, i1, i0) Kernel[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+
+
+    // Insert your GPU convolution kernel code here
+    int W_grid = ceil(W_out / float(TILE_WIDTH));
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    int b = blockIdx.x;
+    int m = blockIdx.y;
+    int h = (blockIdx.z / W_grid) * TILE_WIDTH + ty;
+    int w = (blockIdx.z % W_grid) * TILE_WIDTH + tx;
+
+    if (C == 1) {
+        if ((w < W) && (h < H)) {
+            x_shared_3d(0, ty, tx) = x4d(b, 0, h, w);
+        } else {
+            x_shared_3d(0, ty, tx) = 0.0f;
+        }
+    } else if (C == 4) {
+        if ((w < W) && (h < H)) {
+            x_shared_3d(0, ty, tx) = x4d(b, 0, h, w);
+            x_shared_3d(1, ty, tx) = x4d(b, 1, h, w);
+            x_shared_3d(2, ty, tx) = x4d(b, 2, h, w);
+            x_shared_3d(3, ty, tx) = x4d(b, 3, h, w);
+        } else {
+            x_shared_3d(0, ty, tx) = 0.0f;
+            x_shared_3d(1, ty, tx) = 0.0f;
+            x_shared_3d(2, ty, tx) = 0.0f;
+            x_shared_3d(3, ty, tx) = 0.0f;
+        }
+    }
+    __syncthreads();
+
+
+
+    if ((tx < TILE_WIDTH) && (ty < TILE_WIDTH)) {
+        float acc = 0.0f;
+        for (int c = 0; c < C; c++) {
+            for (int p = 0; p < K; p++) {
+                for (int q = 0; q < K; q++) {
+                    acc += x_shared_3d(c, ty + p, tx + q) * k4d_constant(m, c, p, q);
+                }
+            }
+        }
+        if ((w < W_out) && (h < H_out)) {
+            y4d(b, m, h, w) = acc;
+        }
+    }
+
+
+#undef y4d
+#undef x4d
+#undef k4d
+#undef k4d_shared
+}
+
+
+__host__ void GPUInterface::conv_forward_gpu_prolog(const float *host_y, const float *host_x,
+                                                    const float *host_k, float **device_y_ptr,
+                                                    float **device_x_ptr, float **device_k_ptr,
+                                                    const int B, const int M, const int C,
+                                                    const int H, const int W, const int K) {
+    // Allocate memory and copy over the relevant data structures to the GPU
+    const int H_out = H - K + 1;
+    const int W_out = W - K + 1;
+
+    const unsigned int xSize = B * C * H * W * sizeof(float);
+    const unsigned int ySize = B * M * H_out * W_out * sizeof(float);
+    const unsigned int kSize = M * C * K * K * sizeof(float);
+
+    cudaMalloc((void **)device_x_ptr, xSize);
+    cudaMalloc((void **)device_y_ptr, ySize);
+    cudaMalloc((void **)device_k_ptr, kSize);
+
+    // std::cout << "Successfully allocate cuda memory" << std::endl;
+
+
+    cudaMemcpy(*device_x_ptr, (void *)host_x, xSize, cudaMemcpyHostToDevice);
+    cudaMemcpy(*device_k_ptr, (void *)host_k, kSize, cudaMemcpyHostToDevice);
+    cudaMemcpyToSymbol(Kernel, host_k, kSize, 0, cudaMemcpyHostToDevice);
+
+    // std::cout << "Successfully copy data to cuda memory" << std::endl;
+
+
+
+
+    // We pass double pointers for you to initialize the relevant device pointers,
+    //  which are passed to the other two functions.
+
+    // Useful snippet for error checking
+    // cudaError_t error = cudaGetLastError();
+    // if(error != cudaSuccess)
+    // {
+    //     std::cout<<"CUDA error: "<<cudaGetErrorString(error)<<std::endl;
+    //     exit(-1);
+    // }
+}
+
+
+__host__ void GPUInterface::conv_forward_gpu(float *device_y, const float *device_x,
+                                             const float *device_k, const int B, const int M,
+                                             const int C, const int H, const int W, const int K) {
+    // Set the kernel dimensions and call the kernel
+    const int H_out = H - K + 1;
+    const int W_out = W - K + 1;
+    const int H_grid = ceil(H_out / float(TILE_WIDTH));
+    const int W_grid = ceil(W_out / float(TILE_WIDTH));
+
+    // printf("M: %d\n", M);
+    // printf("C: %d\n", C);
+    // printf("K: %d\n", K);
+    // printf("M * C * K * K: %d\n", M * C * K * K);
+
+
+    dim3 DimGrid(B, M, H_grid * W_grid);
+    dim3 DimBlock(BLOCK_WIDTH, BLOCK_WIDTH, 1);
+
+    conv_forward_kernel<<<DimGrid, DimBlock>>>(device_y, device_x, device_k, B, M, C, H, W, K);
+
+
+    cudaDeviceSynchronize();
+}
+
+
+__host__ void GPUInterface::conv_forward_gpu_epilog(float *host_y, float *device_y, float *device_x,
+                                                    float *device_k, const int B, const int M,
+                                                    const int C, const int H, const int W,
+                                                    const int K) {
+    // Copy the output back to host
+    const int H_out = H - K + 1;
+    const int W_out = W - K + 1;
+    const unsigned int ySize = B * M * H_out * W_out * sizeof(float);
+    cudaMemcpy(host_y, device_y, ySize, cudaMemcpyDeviceToHost);
+
+    // Free device memory
+    cudaFree(device_x);
+    cudaFree(device_y);
+    cudaFree(device_k);
+}
+
+
+__host__ void GPUInterface::get_device_properties() {
+    int deviceCount;
+    cudaGetDeviceCount(&deviceCount);
+
+    for (int dev = 0; dev < deviceCount; dev++) {
+        cudaDeviceProp deviceProp;
+        cudaGetDeviceProperties(&deviceProp, dev);
+
+        std::cout << "Device " << dev << " name: " << deviceProp.name << std::endl;
+        std::cout << "Computational capabilities: " << deviceProp.major << "." << deviceProp.minor
+                  << std::endl;
+        std::cout << "Max Global memory size: " << deviceProp.totalGlobalMem << std::endl;
+        std::cout << "Max Constant memory size: " << deviceProp.totalConstMem << std::endl;
+        std::cout << "Max Shared memory size per block: " << deviceProp.sharedMemPerBlock
+                  << std::endl;
+        std::cout << "Max threads per block: " << deviceProp.maxThreadsPerBlock << std::endl;
+        std::cout << "Max block dimensions: " << deviceProp.maxThreadsDim[0] << " x, "
+                  << deviceProp.maxThreadsDim[1] << " y, " << deviceProp.maxThreadsDim[2] << " z"
+                  << std::endl;
+        std::cout << "Max grid dimensions: " << deviceProp.maxGridSize[0] << " x, "
+                  << deviceProp.maxGridSize[1] << " y, " << deviceProp.maxGridSize[2] << " z"
+                  << std::endl;
+        std::cout << "Warp Size: " << deviceProp.warpSize << std::endl;
+    }
+}
+
+*/
